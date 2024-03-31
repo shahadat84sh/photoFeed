80a0ed1af53cd8aa4bc696ee51be5740c1fda37f
Shahadat Hossain <shahadat84sh@gmail.com>
2024-03-31 16:31:12 +0600

    photofeed practice

---
+++ app/api/photos/[id]/route.js
@@ -0,0 +1,10 @@
+import { getPhotoById } from "@/app/lib/image-data";
+import { NextResponse } from "next/server";
+
+
+export async function GET(request, { params }) {
+    const photoId = params?.id;
+    const data = getPhotoById(photoId);
+
+    return NextResponse.json(data)
+}
